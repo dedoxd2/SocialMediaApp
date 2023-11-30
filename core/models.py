e2352441eea640643c1bfc279581e7d7c356e915
dedoxd2 <dedo.xd33@yahoo.com>
2023-11-30 04:44:34 +0200

    setup the project

---
+++ core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # going to get the model  of the currently authinticated user
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
